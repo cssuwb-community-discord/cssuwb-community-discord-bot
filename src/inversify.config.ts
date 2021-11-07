79608dcdadd81391e17a6b382c766b123af0c702
MSMartin <60079705+MSMartin0@users.noreply.github.com>
2021-11-07 14:35:16 -0800

    Updated container bindings

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -9,10 +9,18 @@ import { PingFinder } from "./services/ping-finder";
 // Dependency Injection Container
 let container = new Container();
 
+// Initialize Discord Bot objects
 container.bind<Bot>(TYPES.Bot).to(Bot).inSingletonScope();
 container.bind<Client>(TYPES.Client).toConstantValue(new Client());
 container.bind<string>(TYPES.DiscordToken).toConstantValue(process.env.DISCORD_TOKEN);
 
+// Initialize Reddit credentials
+container.bind<string>(TYPES.RedditUsername).toConstantValue(process.env.REDDIT_USERNAME);
+container.bind<string>(TYPES.RedditPassword).toConstantValue(process.env.REDDIT_PASSWORD);
+container.bind<string>(TYPES.RedditClientID).toConstantValue(process.env.REDDIT_CLIENT_ID);
+container.bind<string>(TYPES.RedditClientSecret).toConstantValue(process.env.REDDIT_CLIENT_SECRET);
+
+// Initialize Discord Bot functionality classes
 container.bind<MessageResponder>(TYPES.MessageResponder).to(MessageResponder).inSingletonScope();
 container.bind<PingFinder>(TYPES.PingFinder).to(PingFinder).inSingletonScope();
 
