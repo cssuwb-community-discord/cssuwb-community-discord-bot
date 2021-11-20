0e4bafcf43aaf868c053df25112f77bae52a55e2
Compilation <matthewscottmartin2001@gmail.com>
2021-11-20 04:45:46 +0000

    Added Prettier and ESLint

--- src/services/message-responder.ts
+++ src/services/message-responder.ts
@@ -1,23 +1,21 @@
-import {Message} from "discord.js";
-import {PingFinder} from "./ping-finder";
-import {inject, injectable} from "inversify";
-import {TYPES} from "../types";
+import { Message } from "discord.js";
+import { PingFinder } from "./ping-finder";
+import { inject, injectable } from "inversify";
+import { TYPES } from "../types";
 
 // Class to handle how bot should respond to valid user input
 @injectable()
 export class MessageResponder {
   private pingFinder: PingFinder;
 
-  constructor(
-    @inject(TYPES.PingFinder) pingFinder: PingFinder
-  ) {
+  constructor(@inject(TYPES.PingFinder) pingFinder: PingFinder) {
     this.pingFinder = pingFinder;
   }
 
   // Directly handle responding to user here
   handle(message: Message): Promise<Message | Message[]> {
     if (this.pingFinder.isPing(message.content)) {
-      return message.reply('pong!');
+      return message.reply("pong!");
     }
 
     return Promise.reject();
--- src/services/ping-finder.ts
+++ src/services/ping-finder.ts
@@ -1,10 +1,9 @@
-import {injectable} from "inversify";
+import { injectable } from "inversify";
 
 // Class to determine whether a response is a valid ping
 @injectable()
 export class PingFinder {
-
-  private regexp = 'ping';
+  private regexp = "ping";
 
   public isPing(stringToSearch: string): boolean {
     return stringToSearch.search(this.regexp) >= 0;
