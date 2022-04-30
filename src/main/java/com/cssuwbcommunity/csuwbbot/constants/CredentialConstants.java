78c4e124ff1210569f6ecbde2a9958ba0d4b9f63
MSMartin <60079705+MSMartin0@users.noreply.github.com>
2022-04-30 03:03:47 -0700

    Killed token

--- src/main/java/com/cssuwbcommunity/csuwbbot/constants/CredentialConstants.java
+++ src/main/java/com/cssuwbcommunity/csuwbbot/constants/CredentialConstants.java
@@ -4,5 +4,5 @@ public class CredentialConstants {
     public static final String LEETCODE_CHANNEL_ID="885686667325870140";
     public static final String REDDIT_CHANNEL_ID="837167801055576114";
     public static final String HACKERRANK_CHANNEL_ID="951759241440559145";
-    public static final String BOT_TOKEN="OTA2NzkyODA3NDUxMjIyMDI2.YYdynA.KUKy5841KS1EUQ9nM71enQXiBic";
+    public static final String BOT_TOKEN="";
 }
